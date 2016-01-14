79d5ae1df3daff95957789ebb902902fee316859
Nathan Dautenhahn <dautenh1@illinois.edu>
2016-01-14 11:28:56 -0600

    Fix comemnt formatting

--- mm/memorizer/memorizer.c
+++ mm/memorizer/memorizer.c
@@ -26,25 +26,25 @@
  * Redistributions in binary form must reproduce the above copyright notice,
  * this list of conditions and the following disclaimers in the documentation
  * and/or other materials provided with the distribution.  Neither the names of
- * Sam King or the University of Illinois, nor the names of its contributors
- * may be used to endorse or promote products derived from this Software
- * without specific prior written permission. 
+ * Nathan Dautenhahn or the University of Illinois, nor the names of its
+ * contributors may be used to endorse or promote products derived from this
+ * Software without specific prior written permission. 
  *
  * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
  * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
  * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL THE
  * CONTRIBUTORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
- * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
- * WITH THE SOFTWARE. 
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS WITH
+ * THE SOFTWARE.
  *
  *===-----------------------------------------------------------------------===
  *
  *       Filename:  memorizer.c
  *
  *    Description:  Memorizer is a memory tracing tool. It hooks into KASAN
- *                  events to record object allocation/frees and all
- *                  loads/stores. 
+ *		    events to record object allocation/frees and all
+ *		    loads/stores. 
  *
  *===-----------------------------------------------------------------------===
  *
@@ -52,9 +52,9 @@
  *
  *	Memorizer has two global and a percpu data structure:
  *	
- *		- global rbtree of active kernel objects 
- *		- queue for holding free'd objects that haven't logged 
- *		- A percpu event queue to track memory access events
+ *		- global rbtree of active kernel objects - queue for holding
+ *		  free'd objects that haven't logged - A percpu event queue to
+ *		  track memory access events
  *		    
  *     Therefore, we have the following locks:
  *

c211f515727f8687e5c3658a5bd1fcee58b3c585
Nathan Dautenhahn <dautenh1@illinois.edu>
2016-02-02 09:09:26 -0600

    [WIP] lookuptable insert, remove, search
        - First implementation
        - Buggy still
        - The size of the dir and tables are huge. We may need to go to a three
          level lookup table.

--- mm/memorizer/kobj_metadata.c
+++ mm/memorizer/kobj_metadata.c
@@ -49,3 +49,113 @@
  *===-----------------------------------------------------------------------===
  */
 
+#include <linux/gfp.h>
+#include <linux/slab.h>
+
+#include "kobj_metadata.h"
+
+struct lt_dir kobj_dir;
+
+int lt_insert_kobj(struct memorizer_kobj *kobj)
+{
+	struct lt_tbl *tbl;
+	uint64_t dir_i;
+	uint64_t tbl_i;
+	uintptr_t va = kobj->va_ptr;
+	uintptr_t kobjend = kobj->va_ptr + kobj->size;
+
+	while(va < kobjend)
+	{
+		/* find the table from the directory and alloc if needed */
+		dir_i = lt_dir_index(va);
+
+		if (dir_i >= LT_DIR_ENTRIES)
+		{
+			pr_info("Dir index out of bounds\n");
+		}
+
+		tbl = kobj_dir.tbl_ptrs[dir_i];
+
+		if(!tbl){
+			/* allocate table and add to the dir */
+			//tbl = alloc_pages(sizeof(struct lt_tbl), GFP_ATOMIC);
+			extern unsigned long __get_free_pages(gfp_t gfp_mask,
+							      unsigned int
+							      order);
+
+			extern void *alloc_pages_exact(size_t size, gfp_t
+						       gfp_mask);
+			//tbl = (struct lt_tbl *) __get_free_pages(GFP_ATOMIC,
+							 //LT_TBL_SIZE/PAGE_SIZE);
+			tbl = alloc_pages_exact(sizeof(struct lt_tbl)/PAGE_SIZE,
+						GFP_ATOMIC);
+			if(!tbl)
+			{
+				pr_err("failed to allocate table");
+				panic("help");
+				return -1;
+			}
+			memset(tbl,0,sizeof(struct lt_tbl));
+			kobj_dir.tbl_ptrs[dir_i] = tbl;
+		}
+
+		tbl_i = lt_tbl_index(va);
+
+		while(tbl_i < LT_TBL_ENTRIES && va < kobjend)
+		{
+			/* get the pointer to the tbl_entry for this va byte */
+			struct memorizer_kobj **tbl_entry =
+				lt_tbl_entry_get(tbl,tbl_i);
+
+			/* If it is not null then we are double allocating */
+			if(*tbl_entry){
+				pr_err("Cannot insert 0x%lx into lookup table"
+				       " (overlaps existing)\n", kobj->va_ptr);
+				return -1;
+			}
+
+			/* insert the object pointer in the table for byte va */
+			*tbl_entry = kobj;
+
+			/* Track the end of the table and the object tracking */
+			va += 1;
+			++tbl_i;
+		}
+	}
+	return 0;
+}
+
+void lt_remove_kobj(struct memorizer_kobj *kobj)
+{
+	uintptr_t va = kobj->va_ptr;
+	uintptr_t kobjend = kobj->va_ptr + kobj->size;
+	while(va<kobjend)
+	{
+		struct lt_tbl ** dir_entry = lt_dir_entry(&kobj_dir, va);
+		if(!*dir_entry)
+		{
+			pr_info("<free> No table entry");
+			return;
+		}
+		(*dir_entry)->kobj_ptrs[lt_tbl_index(va)] = NULL;
+		va += 1;
+	}
+}
+
+struct memorizer_kobj * lt_get_kobj(uintptr_t va)
+{
+	struct memorizer_kobj *kobj = NULL;
+	struct lt_tbl ** dir_entry = lt_dir_entry(&kobj_dir, va);
+	if(!*dir_entry)
+		return NULL;
+
+	kobj = (*dir_entry)->kobj_ptrs[lt_tbl_index(va)];
+
+	return kobj;
+}
+
+void __init lt_init(void)
+{
+	/* Zero the page dir contents */
+	memset(&kobj_dir, 0, sizeof(kobj_dir));
+}
--- mm/memorizer/kobj_metadata.h
+++ mm/memorizer/kobj_metadata.h
@@ -49,5 +49,116 @@
 
 #ifndef __KOBJ_METADATA_H_
 #define __KOBJ_METADATA_H_
+
+#include <linux/kallsyms.h>
+#include <linux/rbtree.h>
+#include <linux/rwlock.h>
+#include <linux/sched.h>
+
+/** 
+ * struct memorizer_kobj - metadata for kernel objects 
+ * @rb_node:		the red-black tree relations
+ * @alloc_ip:		instruction that allocated the object
+ * @va_ptr:		Virtual address of the beginning of the object
+ * @pa_ptr:		Physical address of the beginning of object
+ * @size:		Size of the object
+ * @jiffies:		Time stamp of creation
+ * @pid:		PID of the current task
+ * @comm:		Executable name
+ * @kobj_list:		List of all objects allocated
+ * @access_counts:	List of memory access count structures
+ *
+ * This data structure captures the details of allocated objects
+ */
+struct memorizer_kobj {
+	struct rb_node	rb_node;
+	rwlock_t	rwlock;
+	long		obj_id;
+	uintptr_t	alloc_ip;
+	uintptr_t	free_ip;
+	uintptr_t	va_ptr;
+	uintptr_t	pa_ptr;
+	size_t		size;
+	unsigned long	alloc_jiffies;
+	unsigned long	free_jiffies;
+	pid_t		pid;
+	char		comm[TASK_COMM_LEN];
+	char		funcstr[KSYM_NAME_LEN];
+	char		*modsymb[KSYM_NAME_LEN];
+	struct list_head	freed_kobjs;
+	struct list_head	access_counts;
+};
+
+/*
+ * Kernel virtual addresses start at ffff880000000000 - ffffc7ffffffffff (=64
+ * TB) direct mapping of all phys. memory --- see
+ * Documentation/x86/x86_64/mm.txt. This means bit 43 is always set, which means
+ * we can remove all bytes where it is unset: TODO Optimization.
+ *
+ * This value is analogous to PAGE_OFFSET
+ *
+ *  63             47 46                      24                      0
+ * +-----------------+---*-------------------+------------------------+
+ * |      ---        |       Directory       |         Table          |
+ * +-----------------+-----------------------+------------------------+
+ *
+ * Right now I assume that all allocation are serviced from the direct mapped
+ * region. This may not be the case as I'm not sure where module allocations
+ * come from.
+ *
+ * So the table effectively maps each byte of allocatable virtual address space
+ * to a pointer to kernel object metadata--> 8 byte pointer.
+ *
+ */
+
+/* 
+ * 2**23 entries * 8 Bytes per  =  67 MB directory table table
+ * 2**24 tables * 8 bytes	= 134 MB Table Size to track 16 MB of VA space
+ */
+#define LT_TBL_MASK_BITS	24
+#define LT_DIR_MASK_BITS	(47 - LT_TBL_MASK_BITS)
+
+#define LT_KADDR_MASK \
+	(~((1UL << (LT_TBL_MASK_BITS + LT_DIR_MASK_BITS)) - 1))
+
+#define LT_TBL_ENTRIES	(_AC(1,UL) << LT_TBL_MASK_BITS)
+#define LT_DIR_ENTRIES	(_AC(1,UL) << LT_DIR_MASK_BITS)
+
+#define LT_TBL_ENTRY_SIZE	(sizeof(void *))
+#define LT_DIR_ENTRY_SIZE	(sizeof(void *))
+
+#define LT_TBL_SIZE	(LT_TBL_ENTRIES * LT_TBL_ENTRY_SIZE)
+#define LT_DIR_SIZE	(LT_DIR_ENTRIES * LT_DIR_ENTRY_SIZE)
+#define LT_PTR_SIZE	sizeof(void *)
+
+struct lt_tbl {
+	struct memorizer_kobj *kobj_ptrs[LT_TBL_SIZE];
+};
+
+struct lt_dir {
+	struct lt_tbl *tbl_ptrs[LT_DIR_ENTRIES];
+};
+
+#define lt_dir_index(va) \
+	(va >> LT_TBL_MASK_BITS) & (LT_DIR_ENTRIES - 1)
+
+inline struct lt_tbl **lt_dir_entry(struct lt_dir *dir, uintptr_t va)
+{
+	return &dir->tbl_ptrs[lt_dir_index(va)];
+}
+
+//inline struct lt_tbl *lt_dir_set(struct lt_dir *dir, uintptr_t va)
+//{
+	//return &(*dir[lt_dir_index(va)]);
+//}
+
+#define lt_tbl_index(va)		va & (LT_TBL_ENTRIES - 1)
+#define lt_tbl_entry_get(tbl,index)	&(tbl->kobj_ptrs[index]);
+
+void lt_init(void);
+int lt_insert_kobj(struct memorizer_kobj *kobj);
+void lt_remove_kobj(struct memorizer_kobj *kobj);
+struct memorizer_kobj * lt_get_kobj(uintptr_t va);
+
 #endif /* __KOBJ_METADATA_H_ */
 

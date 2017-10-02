0f1455ae6e1beae2e427a7129018bd5a1091f796
Jai Pandey <jpandey2@qubit0.cis.upenn.edu>
2017-10-02 12:39:30 -0400

    Memorizer 2.0 First Draft

--- scripts/userApp.c
+++ scripts/userApp.c
@@ -9,15 +9,23 @@
 #include <fcntl.h>
 #include <stdlib.h>
 
-#define ML (100000)   // The size of profiler buffer (Unit: memory page)
+#define ML 100000  // The size of profiler buffer (Unit: memory page)
 #define BUFD_MAX 48000 // The max number of profiled samples stored in the profiler buffer
 
+#define BUFF_MUTEX_LOCK { \
+		while(*buff_mutex); \
+		*buff_mutex = *buff_mutex + 1;\
+	}
+
+#define BUFF_MUTEX_UNLOCK {*buff_mutex = *buff_mutex - 1;}
+
+#define BUFF_FILL_RESET {*buff_fill = 0;}
 
 
 
 static int buf_fd = -1;
 static int buf_len;
-
+struct stat s ;
 
 // This function opens a character device (which is pointed by a file named as fname) and performs the mmap() operation. If the operations are successful, the base address of memory mapped buffer is returned. Otherwise, a NULL pointer is returned.
 void *buf_init(char *fname)
@@ -51,26 +59,63 @@ void buf_exit()
 
 
 
-int main ()
+int main (int argc, char *argv[])
 {
-	unsigned long long *buf;
-	int index = 0;
-	unsigned lonng long i;
+	char *buf;
+	char *buff_end;
+	char *buff_fill;
+	unsigned int *buff_free_size; 
+	unsigned long long index = 0;
+	unsigned long long i;
+	if(argc != 2)
+	{
+		printf("Incorrect number of Command Line Arguments!\n");
+		return 0;
+	}
 
 	// Open the Character Device and MMap 
 	buf = buf_init("node");
 	if(!buf)
 		return -1;
-	//Read and count the MMaped data entries
-	while(buf[index]!=0)
-	{	
-		if(buf[index]==0xaa || buf[index]==0xbb)
-		i++;
-	}
 
+	//Read and count the MMaped data entries
+	buff_end = (buf + ML*getpagesize()) - 1;
+	buff_fill = buf;
+	buf++;
+	buff_free_size = (unsigned int *)buf;
+	buf = buf + sizeof(unsigned int);
+
+	if(*argv[1]=='c')
+	{
+		printf("Remaining Bytes: ");
+		printf("%u",*buff_free_size);
+	}
+	else if(*argv[1]=='p')
+	{
+	//while(1)
+	//{
+		//BUFF_MUTEX_LOCK;
+		//while(!*buff_fill);
+		while(buf+index != buff_end)
+		{
+			printf("%u",buf[index]);
+			index++;
+			*buff_free_size = *buff_free_size + 1;
+			//BUFF_FILL_RESET;
+
+		}
+		/**buff_free_size = *buff_free_size +1;
+		if(buf + index == buff_end)
+			index = 0;
+		else
+			index++;*/
+		//BUFF_MUTEX_UNLOCK;
+	//}
 	
+
+
+	}	
 	buf_exit();
-	printf("%ull",i);	
 	
 	return 0;
 }

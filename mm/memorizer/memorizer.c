18ee4e7f1314c1bb9dfaf9ea737d973ddc484d21
Nathan Dautenhahn <ndd@cis.upenn.edu>
2018-11-27 13:13:08 -0500

    Check+account for induced object on missing kobj
    Signed-off-by: Nathan Dautenhahn <ndd@cis.upenn.edu>

--- mm/memorizer/memorizer.c
+++ mm/memorizer/memorizer.c
@@ -747,12 +747,20 @@ static inline int find_and_update_kobj_access(uintptr_t src_va_ptr,
         kobj = lt_get_kobj(va_ptr);
 
         if(!kobj){
-                enum AllocType AT = kasan_obj_type(va_ptr,size);
-                kobj = general_kobjs[AT];
-                if(AT == MEM_STACK_PAGE)
-                        track_access(AT);
-                else
-                        track_untracked_access(AT);
+                if(is_induced_obj(va_ptr))
+                {
+                        kobj = general_kobjs[MEM_INDUCED];
+                        track_access(MEM_INDUCED);
+                }
+                else{
+
+                        enum AllocType AT = kasan_obj_type(va_ptr,size);
+                        kobj = general_kobjs[AT];
+                        if(AT == MEM_STACK_PAGE)
+                                track_access(AT);
+                        else
+                                track_untracked_access(AT);
+                }
         }
         else
         {

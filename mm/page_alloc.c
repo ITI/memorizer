7ce2c9817c66582e020defe5f4b05d2f619a8841
Nick Roessler <nroess@seas.upenn.edu>
2019-05-08 13:25:53 -0400

    working on get_freepages allocator

--- mm/page_alloc.c
+++ mm/page_alloc.c
@@ -3877,11 +3877,18 @@ unsigned long __get_free_pages(gfp_t gfp_mask, unsigned int order)
 	 * __get_free_pages() returns a 32-bit address, which cannot represent
 	 * a highmem page
 	 */
+	
+	
 	VM_BUG_ON((gfp_mask & __GFP_HIGHMEM) != 0);
 
+	memorizer_start_getfreepages();
+	
 	page = alloc_pages(gfp_mask, order);
 	if (!page)
 		return 0;
+	
+	memorizer_alloc_getfreepages(_RET_IP_, page, order, gfp_mask);
+	
 	return (unsigned long) page_address(page);
 }
 EXPORT_SYMBOL(__get_free_pages);

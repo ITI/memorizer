f1c3e449ef6b771426f4d9e8790bb79ce84caf38
Rob Adams <robadams@illinois.edu>
2023-04-24 11:09:19 -0500

    Organize the code in preparation for adding the "drip" test style.
    Former-commit-id: 73bc9eeebbd023f491da505ba5a20c98654493c2

--- VM/mkosi.skeleton/doit.sh
+++ VM/mkosi.skeleton/doit.sh
@@ -1,48 +1,121 @@
 #!/bin/bash -ex
 
+# Default values, can be overridden by arguments
 U=robadams
 D="/data/$U/data/$(uname -r)/$(date -Iseconds)"
-ssh "$U@_gateway" mkdir -p "$D"
+H="_gateway"
 
+MZ=/sys/kernel/debug/memorizer
+TESTTYPE=dump
+
+function create_dir() {
+  ssh "$U@$H" mkdir -p "$D"
+}
+
+copy_iter=0
 function copy() {
   # Stoopid virtual file system. scp won't copy
   # the file directly because its size is 0.
   # Here's hoping there aren't any spaces in $D, $1, or $2
-  cat "$1" | ssh $U@_gateway "cat > $D/${2:-$1}"
+  cat "$1" | ssh $U@_gateway "cat >> '$D/${2:-$1}.${copy_iter}'"
+  copy_iter=$(( $copy_iter + 1 ))
 }
 
-cd /sys/kernel/debug/memorizer
+function setup() {
+  cd $MZ
 
-# Turn everything off and clear stale data
-echo 0 > memorizer_enabled
-echo 0 > memorizer_log_access
-echo 0 > cfg_log_on
-echo 0 > cfgmap
-echo 1 > clear_dead_objs
-# Adjust following line: 0 gets less data, 1 gets more data
-echo 1 > print_live_obj
-echo 1 > clear_printed_list
+  # Are we in the right spot?
+  cat < memorizer_enabled > /dev/null
 
-# Given above config, kmap should be empty
-# [[ $(wc -l <kmap) == 0 ]]
+  # Turn everything off and clear stale data
+  echo 0 > memorizer_enabled
+  echo 0 > memorizer_log_access
+  echo 0 > cfg_log_on
+  echo 0 > cfgmap
+  echo 1 > clear_dead_objs
+  # Adjust following line: 0 gets less data, 1 gets more data
+  echo 1 > print_live_obj
+  echo 1 > clear_printed_list
+ }
 
-# Turn everything on
-echo 1 > cfg_log_on
-echo 1 > memorizer_enabled
-echo 1 > memorizer_log_access
+function on() {
+  # Turn everything on
+  echo 1 > cfg_log_on
+  echo 1 > memorizer_enabled
+  echo 1 > memorizer_log_access
+}
 
-# Run some test.
-"$@"
-
-# Turn everything off
-echo 0 > memorizer_enabled
-echo 0 > memorizer_log_access
-echo 0 > cfg_log_on
+function off() {
+  # Turn everything off
+  echo 0 > memorizer_enabled
+  echo 0 > memorizer_log_access
+  echo 0 > cfg_log_on
+}
 
 # If we gathered any data, kmap should be non-empty
 # [[ $(wc -l <kmap) != 0 ]]
 
 # Store the data on the server
-copy kmap
-copy cfgmap
-copy global_table
+function copy_all() {
+  copy kmap
+  copy cfgmap
+  copy global_table
+}
+
+function dump() {
+  create_dir
+  setup
+  on
+  "$@"
+  off
+  copy_all
+}
+
+function stream() {
+  echo Not implemented yet
+  false
+}
+
+### Main entry point
+
+# First, parse a bunch of args
+
+while getopts 't:u:h:' opt; do
+  case "$opt" in
+    u)
+      U="$OPTARG"
+      ;;
+    h)
+      H="$OPTARG"
+      ;;
+    t)
+      TESTTYPE="$OPTARG"
+      ;;
+    ?)
+      echo "Usage: $(basename 0) -s -u USER -h HOST -t dump|drip|stream"
+      exit 1
+      ;;
+  esac
+done
+shift "$(($OPTIND -1))"
+
+# Now lanch the test
+case "$TESTTYPE" in 
+  dump )
+    dump "$@"
+    ;;
+  stream )
+    stream "$@"
+    ;;
+  drip )
+    drip "$@"
+    ;;
+  * )
+    echo unknown stream type "$TESTTYPE"
+    exit 1
+    ;;
+esac
+
+exit 0
+
+

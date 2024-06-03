d418fd491da3365d8da100d48757f3b0fcaa80fa
hpoggie <hpoggie@illinois.edu>
2024-06-03 10:14:25 -0500

    Run the old memorizer test in the new system

--- scripts/memorizer/test/root/test/mztest.bat
+++ scripts/memorizer/test/root/test/mztest.bat
@@ -12,3 +12,18 @@
 	grep -q "^1" /sys/kernel/debug/memorizer/memorizer_enabled
 	echo -n 0 > /sys/kernel/debug/memorizer/memorizer_enabled
 }
+
+@test "ls" {
+	echo -n 1 > /sys/kernel/debug/memorizer/clear_dead_objs
+	cp /sys/kernel/debug/memorizer/kmap /dev/null
+	echo -n 1 > /sys/kernel/debug/memorizer/clear_printed_list
+	echo -n 0 > /sys/kernel/debug/memorizer/print_live_obj
+	echo -n 1 > /sys/kernel/debug/memorizer/memorizer_log_access
+	echo -n 1 > /sys/kernel/debug/memorizer/memorizer_enabled
+	ls -l /sys/kernel/debug/memorizer
+	echo -n 0 > /sys/kernel/debug/memorizer/memorizer_enabled
+	echo -n 0 > /sys/kernel/debug/memorizer/memorizer_log_access
+	cp /sys/kernel/debug/memorizer/kmap /root/kmap
+	ls /root/
+	echo done
+}
